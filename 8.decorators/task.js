07f1ea0134ffe602da4de6589af8a7630f8816a8
Margarita May Lee <ritochka.m_lee@yahoo.com>
2022-10-30 18:11:00 +0300

    last task has been done

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,48 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+  function wrapper(...args) {
+    let hash = args.join(',');
+    let currentResult = cache.filter(cacheRecord => cacheRecord.hash === hash);
+    if (currentResult.length === 1) {
+        console.log('Из кэша: ' + currentResult[0].value);
+        return 'Из кэша: ' + currentResult[0].value;
+    } 
+    else {
+      let value = func.call(this, ...args);
+      console.log('Вычисляем: ' + value);
+      if (cache.length < 5) {   
+        cache.push({hash, value});
+      } 
+      else {
+        cache.unshift({hash, value});
+        cache.pop();
+      } 
+      return 'Вычисляем: ' + value;
+    }
+  }
+  return wrapper;
 }
 
+function debounceDecoratorNew(func, ms) {
+  let timeout;
+  func(...rest);
+  let flag = true;
+  return function (...rest) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      if (!flag) {
+        func.call(this, ...rest); 
+        flag = true;
+      }
+    }, ms);
+  };   
+}
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecorator2(debounceDecoratorNew) {
+  let count = 0;
+  function wrapper(...rest) {
+    wrapper.history = count++;
+    return debounceDecoratorNew.call(this, ...rest);
+  }
+  return wrapper;
 }
